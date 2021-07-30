1f2462c945de3160213e42d7164650075a2eb193
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-07-30 13:22:25 +0200

    Architecture tests - Generic code rule

--- clean-arch-2-8-5-master/src/test/java/com/smalaca/rentalapplication/architecture/PackageStructureTest.java
+++ clean-arch-2-8-5-master/src/test/java/com/smalaca/rentalapplication/architecture/PackageStructureTest.java
@@ -1,8 +1,6 @@
 package com.smalaca.rentalapplication.architecture;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
-import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 import org.junit.jupiter.api.Test;
 
@@ -10,10 +8,7 @@ class PackageStructureTest {
 
     @Test
     void domainShouldTalkOnlyWithDomain(){
-        final JavaClasses javaClasses = new ClassFileImporter()
-                .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_JARS)
-                .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
-                .importPackages("com.smalaca.rentalapplication");
+        final JavaClasses javaClasses = RentalApplicationClasses.get();
 
         ArchRuleDefinition.classes()
                 .that().resideInAnyPackage("..domain..")
