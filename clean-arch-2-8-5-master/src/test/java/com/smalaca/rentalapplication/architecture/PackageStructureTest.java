060bee5cc620c39da730a4ffdfcef40461540432
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-07-30 14:07:47 +0200

    Architecture tests - verify if package separation is fine

--- clean-arch-2-8-5-master/src/test/java/com/smalaca/rentalapplication/architecture/PackageStructureTest.java
+++ clean-arch-2-8-5-master/src/test/java/com/smalaca/rentalapplication/architecture/PackageStructureTest.java
@@ -17,4 +17,37 @@ class PackageStructureTest {
 
     }
 
+    @Test
+    void applicationShouldTalkOnlyWithDomainAndApplication(){
+        final JavaClasses javaClasses = RentalApplicationClasses.get();
+
+        ArchRuleDefinition.classes()
+                .that().resideInAnyPackage("..application..")
+                .should().onlyAccessClassesThat().resideInAnyPackage("..domain..","..application..","java..")
+                .check(javaClasses);
+    }
+
+    @Test
+    void queryShouldTalkOnlyWithQuery(){
+        final JavaClasses javaClasses = RentalApplicationClasses.get();
+
+        ArchRuleDefinition.classes()
+                .that().resideInAnyPackage("..query..")
+                .should().onlyAccessClassesThat().resideInAnyPackage("..query..","java..")
+                .check(javaClasses);
+    }
+
+    @Test
+    void  infrastructureShouldTalkOnlyWithInfrastructure(){
+        final JavaClasses javaClasses = RentalApplicationClasses.get();
+
+        ArchRuleDefinition.classes()
+                .that().resideInAnyPackage("..infrastructure..")
+                .should().onlyAccessClassesThat().resideOutsideOfPackage("..domain..")
+                .orShould().haveSimpleNameContaining("Repository")
+                .check(javaClasses);
+
+    }
+
+
 }
